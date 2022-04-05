11e59c86fce3b045e31ed87011688ab407bc12d0
Lucca Tommaso Monzani <lucca.monzani@gmail.com>
2022-04-05 18:05:35 -0300

    ADC certa funcionando

--- Inc/ADC.hpp
+++ Inc/ADC.hpp
@@ -9,26 +9,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define sens_num 10
+
 class ADC {
+private:
+	ADC_HandleTypeDef *hadc;
+	uint32_t valores[sens_num];
+	uint32_t buffer[10];
 
 public:
-	ADC(uint8_t sens_number, ADC_HandleTypeDef *hadcx);
+	ADC( ADC_HandleTypeDef *hadcx);
 	~ADC();
-	uint8_t Get_sens_num();
 	ADC_HandleTypeDef* Get_chanel();
-	void Set_sens_num(uint8_t sens_number);
-	uint32_t *Get_sens_vec_val();
-	void Set_sens_vec();
+	uint32_t *Get_valores();
 	uint32_t* Get_buffer();
 	void Set_buffer();
 	void Ler_DMA();
 	void Parar_DMA();
 
-private:
-	uiunt8_t sens_num;
-	ADC_HandleTypeDef *hadc;
-	uint32_t sens_vec_val[sens_num];
-	uint32_t buffer[10];
+
 
 };
 
--- Src/ADC.cpp
+++ Src/ADC.cpp
@@ -1,32 +1,22 @@
 #include "ADC.hpp"
 
-#ifdef HAL_ADC_MODULE_ENABLED
+#ifdef TAL_ADC
 
-ADC::ADC(uint8_t sens_number, ADC_HandleTypeDef* hadcx) {
+ADC::ADC(ADC_HandleTypeDef* hadcx) {
 	this->hadc = hadcx;
 	// TODO Auto-generated constructor stub
-
 }
 
 ADC::~ADC() {
 	// TODO Auto-generated destructor stub
 }
 
-uint8_t ADC::Get_sens_num() {
-	return this->sens_num;
-}
-
-void ADC::Set_sens_num(uint8_t sens_number) {
-	this->sens_num = sens_number;
-}
-
-
 ADC_HandleTypeDef* ADC::Get_chanel() {
 	return this->hadc;
 }
 
-uint32_t* ADC::Get_sens_vec_val() {
-	return this->sens_vec_val;
+uint32_t* ADC::Get_valores() {
+	return this->valores;
 }
 
 uint32_t* ADC::Get_buffer() {
@@ -41,7 +31,7 @@ void ADC::Ler_DMA() {
 void ADC::Parar_DMA() {
 	HAL_ADC_Stop_DMA(this->hadc);
 	free(this->buffer);
-	free(this->sens_vec_val);
+	free(this->valores);
 }
 
 

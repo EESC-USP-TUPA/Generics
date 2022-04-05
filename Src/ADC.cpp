dc315404fad15b2433dd95cd5eea1bc0390151a0
Lucca Tommaso Monzani <lucca.monzani@gmail.com>
2022-04-05 16:09:41 -0300

    Cosertando classe adc

--- Src/ADC.cpp
+++ Src/ADC.cpp
@@ -2,7 +2,7 @@
 
 #ifdef HAL_ADC_MODULE_ENABLED
 
-ADC::ADC(uint8_t sens_number, ADC_HandleTypeDef *hadcx) {
+ADC::ADC(uint8_t sens_number, ADC_HandleTypeDef* hadcx) {
 	this->hadc = hadcx;
 	// TODO Auto-generated constructor stub
 
@@ -25,26 +25,24 @@ ADC_HandleTypeDef* ADC::Get_chanel() {
 	return this->hadc;
 }
 
-uint32_t *ADC::Get_sens_vec_val(){
+uint32_t* ADC::Get_sens_vec_val() {
 	return this->sens_vec_val;
 }
 
-uint32_t* ADC::Get_buffer(){
+uint32_t* ADC::Get_buffer() {
 	return this->buffer;
 }
 
 
 void ADC::Ler_DMA() {
-	for(uint8_t i = 0; i < SENS_NUM; i++){
-		HAL_ADC_Start_DMA(this->hadc, this->buffer, 10);
-		this->sens_vec_val[i] = Media_movel(this->buffer, 10);
-	}
+	HAL_ADC_Start_DMA(this->hadc, this->buffer, 10);
 }
 
-void ADC::Parar_DMA(){
+void ADC::Parar_DMA() {
 	HAL_ADC_Stop_DMA(this->hadc);
 	free(this->buffer);
 	free(this->sens_vec_val);
 }
+
 
 #endif
